218b66155fa758b416a876b5c886773d37429593
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-07-29 20:30:08 +0300

    Create a function that gets user input

--- rock-paper-scissors/jscode.js
+++ rock-paper-scissors/jscode.js
@@ -1,3 +1,14 @@
+// Take user input and compare with generator result
+function getInputValueAndCompare() {
+    let userInput = document.getElementById("user-input").value;
+    console.log(userInput);
+
+    let generatorInput = randomGenerator();
+    console.log(generatorInput);
+
+
+}
+
 // Random rock-paper-scissors generator
 function randomGenerator() {
     
@@ -6,9 +17,6 @@ function randomGenerator() {
 
     condition = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 
-    console.log(`Generator is ${generatorArray[condition]}`);
+    return generatorArray[condition];
 }
 
-randomGenerator();
-
-console.log("aba" === "aBa".toLowerCase());
